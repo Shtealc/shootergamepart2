bcac1b2fc634c225750dd47860e67f3b4d1ee01a
an14988 <an567336@ucf.edu>
2024-11-10 19:16:55 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public GameObject enemy;
     public GameObject cloud;
+    public GameObject coin;
     private int score;
 
     public TextMeshProUGUI scoreText;
@@ -19,6 +20,7 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateCoin", 1f, 3f);
         CreateSky();
         score = 0;
         scoreText.text = "Score: " + score;
@@ -34,7 +36,10 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
     }
-
+    void CreateCoin()
+    {
+        Instantiate(coin, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
+    }
     void CreateSky()
     {
         for (int i = 0; i < 30; i++)
